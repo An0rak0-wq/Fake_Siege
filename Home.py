5b08dd383a15e16d16a7f6dd1146c7380ac908ba
Callum Rawson <22rawsoncal@student.bournemouth-school.org>
2025-06-18 17:22:58 +0100

    1. Added an info section to Home.py 2. Created a barebones mennu system for Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,9 @@
+############
+### INFO ###
+############
+
+#Here is where is will place any info that needs to be read before execution. This can also be found in the README.
+
 ###############
 ### IMPORTS ###
 ###############
@@ -8,12 +14,32 @@ import random
 ### VARIABLE INITIALISATION ###
 ###############################
 
-
+exit = False
 
 #################
 ### FUNCTIONS ###
 #################
 
+def menu():
+    print("\nWelcome to Besiegement!")
+    choice = input("1) Play\n"
+                   "2) Operators\n"
+                   "3) Settings\n"
+                   "4) Exit\n")
+    
+    if choice == "1":
+            print("Play")
+    elif choice == "2":
+            print("Operators")
+    elif choice == "3":
+            print("Settings")
+    elif choice == "4":
+            print("Exit")
+
 ############
 ### MAIN ###
 ############
+
+
+while exit != True:
+    menu()
